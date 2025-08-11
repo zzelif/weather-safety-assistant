8ba00a7b4d08e879095e181c6837a8129f664c20
dan <dan.lettac@gmail.com>
2025-08-12 00:58:08 +0800

    add: shadcn blocks and dependencies

---
+++ src/app/api/weather/route.ts
@@ -0,0 +1,6 @@
+/* import { NextResponse } from "next/server";
+import axios from "axios";
+
+export async function GET() {
+    const url = `http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHERAPI_API_KEY}`
+} */
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const interSans = Inter({
+  variable: "--font-inter-sans",
   subsets: ["latin"],
 });
 
@@ -24,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${interSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
